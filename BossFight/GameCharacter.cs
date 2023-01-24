42033e26b50a34b54950cb01b0a2e270dd2d5483
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-24 23:38:50 +0100

    Add project files.

---
+++ BossFight/GameCharacter.cs
@@ -0,0 +1,53 @@
+﻿
+namespace BossFight
+{
+    internal class GameCharacter
+    {
+        private string _name;
+        private int _health;
+        private int _maxStrength;
+        private int _stamina;
+        public GameCharacter(string name, int health, int strength, int stamina)
+        {
+            _name = name;
+            _health = health;
+            _maxStrength = strength;
+            _stamina = stamina;
+        }
+        public void Fight(GameCharacter opponentCharacter)
+        {
+            if (_stamina != 0)
+            {
+                var randomAttackDmg = RandomStrengthAttack();
+                opponentCharacter._health -= randomAttackDmg;
+                _stamina -= 10;
+                Console.WriteLine(_name 
+                                  + " hits " 
+                                  + opponentCharacter._name 
+                                  + " with " + randomAttackDmg 
+                                  + "," + opponentCharacter._name 
+                                  + " now has" + opponentCharacter._health 
+                                  + "left");
+            }
+            else
+            {
+                Recharge();
+                Console.WriteLine(_name + "has no stamina left! Recharge used instead of attack!");
+            }
+        }
+        private int RandomStrengthAttack()
+        {
+            var random = new Random();
+            return random.Next(0, _maxStrength);
+        }
+
+        public void Recharge()
+        {
+            _stamina += 10;
+        }
+        public void Show()
+        {
+            Console.WriteLine(_name);
+        }
+    }
+}
---
+++ BossFight/Program.cs
@@ -0,0 +1,3 @@
+﻿using BossFight;
+
+var boss = new GameCharacter("Boss",400, 30, 10);
