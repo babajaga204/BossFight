db516fa1ee3a2bc2666e2f9d5d025614538b92dd
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-25 09:16:17 +0100

    working fight loop

--- BossFight/GameCharacter.cs
+++ BossFight/GameCharacter.cs
@@ -3,36 +3,59 @@ namespace BossFight
 {
     internal class GameCharacter
     {
-        private string _name;
-        private int _health;
-        private int _maxStrength;
-        private int _stamina;
-        public GameCharacter(string name, int health, int strength, int stamina)
+        public string _name;
+        public int _health;
+        public int _maxStrength;
+        public int _stamina;
+        public bool _isBoss;
+        public GameCharacter(string name, int health, int strength, int stamina, bool isBoss)
         {
             _name = name;
             _health = health;
             _maxStrength = strength;
             _stamina = stamina;
+            _isBoss = isBoss;
         }
         public void Fight(GameCharacter opponentCharacter)
         {
             if (_stamina != 0)
             {
-                var randomAttackDmg = RandomStrengthAttack();
-                opponentCharacter._health -= randomAttackDmg;
-                _stamina -= 10;
-                Console.WriteLine(_name 
-                                  + " hits " 
-                                  + opponentCharacter._name 
-                                  + " with " + randomAttackDmg 
-                                  + "," + opponentCharacter._name 
-                                  + " now has" + opponentCharacter._health 
-                                  + "left");
+                if (_isBoss)
+                {
+                    var randomAttackDmg = RandomStrengthAttack();
+                    opponentCharacter._health -= randomAttackDmg;
+                    _stamina -= 10;
+                    Console.WriteLine(_name
+                                      + " hits "
+                                      + opponentCharacter._name
+                                      + " with "
+                                      + randomAttackDmg
+                                      + " damage,"
+                                      + opponentCharacter._name
+                                      + " now has " + opponentCharacter._health
+                                      + " health left");
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    opponentCharacter._health -= _maxStrength;
+                    _stamina -= 10;
+                    Console.WriteLine(_name
+                                      + " hits "
+                                      + opponentCharacter._name
+                                      + " with "
+                                      + _maxStrength
+                                      + "," + opponentCharacter._name
+                                      + " now has " + opponentCharacter._health
+                                      + " health left");
+                    Thread.Sleep(1000);
+                }
             }
             else
             {
                 Recharge();
-                Console.WriteLine(_name + "has no stamina left! Recharge used instead of attack!");
+                Console.WriteLine(_name + " has no stamina left! Recharge used instead of attack!");
+                Thread.Sleep(1000);
             }
         }
         private int RandomStrengthAttack()
@@ -47,7 +70,13 @@ namespace BossFight
         }
         public void Show()
         {
-            Console.WriteLine(_name);
+            Console.WriteLine(@$"Status: 
+Name: {_name}
+Health: {_health}
+Stamina: {_stamina}
+Strength: {_maxStrength}
+");
+            Console.WriteLine();
         }
     }
 }
--- BossFight/Program.cs
+++ BossFight/Program.cs
@@ -1,3 +1,22 @@
 ﻿using BossFight;
 
-var boss = new GameCharacter("Boss",400, 30, 10);
+var boss = new GameCharacter("Boss",400, 30, 10, true);
+var hero = new GameCharacter("Jake", 100, 20, 40, false);
+boss.Show();
+hero.Show();
+while (true)
+{
+    hero.Fight(boss);
+    boss.Fight(hero);
+    Console.WriteLine();
+    if (hero._health <= 0 || boss._health <= 0)
+    {
+        if (hero._health <= 0)
+        {
+            Console.WriteLine("Boss wins the game!");
+            break;
+        } 
+        Console.WriteLine("Hero wins the game!");
+        break;
+    }
+}
